d8c02af2c115d8c4e290c96a2fdf87bb4595ccbb
Ruslan <randomrocketgalaxy@gmail.com>
2025-06-22 00:15:16 +0300

    Fixed type def of video hook

--- src/components/Reels.tsx
+++ src/components/Reels.tsx
@@ -43,7 +43,7 @@ export default function Reels(){
   })
 
   // const [selectedVideo, setSelectedVideo] = useState<any>(null);
-  const [videos, setVideos] = useState<any[]>([]); 
+  const [videos, setVideos] = useState<FormattedVideo[]>([]); 
   // const [isLoadingPlaylist, setIsLoadingPlaylist] = useState(false)
   // const [playlistError, setPlaylistError] = useState<string | null>(null)
 

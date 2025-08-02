1e888af47311f202549fbce581d3435f6110ddc5
Ruslan <randomrocketgalaxy@gmail.com>
2025-08-02 13:58:02 +0300

    ADD added songs to tracklist

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
     return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/svg+xml" href="/assets/BIG-LOGO-SD.png" />
+        <link rel="icon" type="image/svg+xml" href="/assets/Mindless_Logo.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" /> {/* crossorigin */}
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/>
